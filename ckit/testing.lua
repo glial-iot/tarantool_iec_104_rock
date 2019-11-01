5709b6aa0486ffe5a7104ba3ae7380ad8e1f4916
Nikolay Matyukhin <matuhin86@gmail.com>
2019-11-02 02:31:36 +0300

    added testing file

---
+++ ckit/testing.lua
@@ -0,0 +1,22 @@
+local iec_104 = require('ckit.lib')
+
+libsocket = require "socket"
+libunix = require "socket.unix"
+socket = assert(libunix())
+
+SOCKET_FILE = "/tmp/socket"
+os.remove(SOCKET_FILE)
+assert(socket:bind(SOCKET_FILE))
+assert(socket:listen())
+
+iec_104.fetch("meter1.example.com", 2404, SOCKET_FILE, true);
+iec_104.fetch("meter2.example.com", 2404, SOCKET_FILE, true);
+while true do
+  conn = assert(socket:accept())
+  data=conn:receive("*a") -- receive all data from socket, until connection is closed
+  if (data ~= nil) then
+    print("Got data: " .. data)
+  else
+    print("Got no data!")
+  end
+end
