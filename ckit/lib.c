ec634d2bf98ca23d1d65cf3bf4a7e5dd306421ab
Aleksey Nemcev <Aleksey.Nemcev.ext@nokia.com>
2019-11-13 11:58:57 +0300

    ckit: Remove redurant NULL-check in report_measurements()

--- ckit/lib.c
+++ ckit/lib.c
@@ -199,10 +199,6 @@ void report_measurements(struct context *context) {
         return;
     }
     const char *json_string = json_object_get_string(context->master_object);
-    if (context->master_object == NULL) {
-        fprintf(stderr, "%s:%i ERROR: Unable to report data - can't serialize master_object!\n", context->host, context->port);
-        return;
-    }
     printf("%s:%i Reporting data: %s\n", context->host, context->port, json_string);
     bool reported;
     int retries = 0;
