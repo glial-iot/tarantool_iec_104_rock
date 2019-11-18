87c52f257660f4337c8482ad5db8dcdb7116cb25
Aleksey Nemcev <Aleksey.Nemcev.ext@nokia.com>
2019-11-18 14:41:32 +0300

    ckit: Replace fetch to meter_add in testing_tcp.lua

--- ckit/testing_tcp.lua
+++ ckit/testing_tcp.lua
@@ -5,8 +5,8 @@ local socket = require "socket"
 local sock = socket.bind('127.0.0.1', 0)
 address, port = sock:getsockname()
 
-iec_104.meter_add("meter1.example.com", 2404, port, true);
-iec_104.meter_add("meter2.example.com", 2404, port, false);
+iec_104.meter_add("meter1.example.com", 2404, port, true)
+iec_104.meter_add("meter2.example.com", 2404, port, false)
 while true do
     conn = assert(sock:accept())
     data = conn:receive("*a") -- receive all data from socket, until connection is closed
